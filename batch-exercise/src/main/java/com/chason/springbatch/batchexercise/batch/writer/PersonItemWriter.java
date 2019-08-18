5644937c18244814bc697ab332d50fc13335cb27
cangqiong <cang19910623@163.com>
2019-08-18 14:05:19 +0800

    添加批量的代码

---
+++ batch-exercise/src/main/java/com/chason/springbatch/batchexercise/batch/writer/PersonItemWriter.java
@@ -0,0 +1,48 @@
+package com.chason.springbatch.batchexercise.batch.writer;
+
+import com.chason.springbatch.batchexercise.entity.People;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * 写数据
+ * Author: chason
+ * Date: 2019/8/18 11:49
+ **/
+@Slf4j
+public class PersonItemWriter implements ItemWriter<People> {
+
+    private ItemWriter writer;
+
+    private DataSource dataSource;
+
+    public PersonItemWriter(DataSource dataSource) {
+        this.dataSource = dataSource;
+        writer = new JdbcBatchItemWriterBuilder<People>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO people (first_name, last_name) VALUES (?,?)")
+                .itemPreparedStatementSetter(new ItemPreparedStatementSetter<People>() {
+                    @Override
+                    public void setValues(People item, PreparedStatement ps) throws SQLException {
+                        ps.setString(1, item.getFirstName());
+                        ps.setString(2, item.getLastName());
+                    }
+                })
+                .dataSource(dataSource)
+                .build();
+    }
+
+    @Override
+    public void write(List<? extends People> items) throws Exception {
+        log.info("Once write person size :{}",items.size());
+        writer.write(items);
+    }
+}
